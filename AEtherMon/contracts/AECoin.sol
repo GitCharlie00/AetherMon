ed6ec1366f008e274923180049cf18d94a5d8050
Mattia Russo <russo.1918968@studenti.uniroma1.it>
2024-01-22 15:44:52 +0100

    Added SCs

---
+++ AEtherMon/contracts/AECoin.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.18;
+
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./Owner.sol";
+
+contract AECoinFT is ERC20Burnable,Owner{
+    //*--------------State variables--------------
+    uint8 private reward;                                       //The number of token received as reward at the end of a battle
+
+    address public gameContractAddress;                         //The address of the contract that manages all the dapp
+
+    //Chiamo il costruttore di ERC20 settando nome e simbolo per il token
+    constructor(address _gameContractAddress) ERC20("AECoin", "AEC") {
+        reward = 10;     
+        gameContractAddress = _gameContractAddress;       
+    }   
+
+
+    //*--------------Modifiers--------------
+    modifier onlyGameContract(){
+        require(msg.sender == gameContractAddress,"Only the game contract can call this function");
+        _;
+    }
+
+    //*--------------Events--------------
+    event Purchase(address buyer, uint256 amount);           //Evento quando l'utente acquista nuove monete
+    event Burn(address account, uint256 amount);            //Evento quando l'utente spende nuove monete
+    event Win(address account, uint256 amount);
+
+    //*--------------Main functionalities--------------
+
+    //Function to mint coin for a user
+    function purchaseCoins(address buyer,uint256 numberOfCoins) public onlyGameContract{
+        //Emette nuove monete per l'acquirente
+        _mint(buyer, numberOfCoins);
+
+        //Emessione evento di acquisto
+        emit Purchase(msg.sender, numberOfCoins);
+    }
+
+    //Needed to allow the payment between players when they exchange monsters using the market
+    function transferCoinsFrom(address buyer, address seller, uint256 amount) public onlyGameContract{
+        _transfer(buyer, seller, amount);
+    }
+
+    //Funzione da chiamare quando un utente vince una battaglia ( e quindi guadagna monete)
+    function getReward(address winner) external onlyGameContract{
+        _mint(winner, reward);
+        emit Win(winner, reward);
+    }     
+
+    
+    //Utile quando un utente spende i token                         
+    function burnFromGame(address account, uint256 amount) external onlyGameContract {
+        // Chiama la funzione burn di ERC20Burnable
+        _burn(account, amount);    
+        emit Burn(account, amount);         
+    }
+
+
+}
---
+++ AEtherMon/contracts/Game.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.18;
+
+import "./AEMarket.sol";            //NOTE: This contract also deploys the AECoinFT contract and the AEMonsterNFT contract
+
+contract GameContract is AEMarket{
+    //*--------------State variables--------------
+
+    //The owner of the contract
+    address public owner;
+
+    //*The constructor define the account to interact with
+    constructor() AEMarket(){
+        owner = msg.sender;
+    }
+
+    //*--------------Modifiers--------------
+
+    //It verifies if a player has enough AECoin to buy a new AEthermon
+    modifier canBuyNewMonster(address player){
+        require(AECoinContractAddress.balanceOf(player) >= 100,"Not enough AECoins");
+        _;
+    }
+
+    //*--------------Main functionalities--------------
+
+    //NOTE : Ricorda che in Ethereum viene utilizzato il Wei ( 1eth = 1e18wei = 10^18wei)
+    //NOTE : Ricorda che msg.value è espresso in wei
+    //Funzione che consente all'utente di acquistare monente di gioco (100AEC = 0.005 ETH) => 1Coin = (0.005 / 100 eth) = 0.00005 eth
+    function purchaseGameCoins(uint256 numberOfCoins) public payable{
+        //Calcola il costo in eth delle monete richieste
+        uint256 costInEth = (numberOfCoins * 5e13); 
+
+        //Verifica che l'importo inviato sia sufficente
+        require(msg.value >= costInEth, "Insufficient ETH sent");
+
+        AECoinContractAddress.purchaseCoins(msg.sender, numberOfCoins);
+
+        //Invia i guadagni al proprietario del contratto
+        payable(owner).transfer(msg.value);                     //*Se metti più soldi li perdi :)
+    }
+
+
+    //*Retrive the number of coins of a player
+    function coinBalanceOf(address player) public view returns (uint256){
+        return AECoinContractAddress.balanceOf(player);
+    }
+
+
+    //*Purchase a new monster function      //NOTE: A new monster costs 100 AECoin
+    function buyNewMonster(string calldata tokenURI) canBuyNewMonster(msg.sender) external returns(uint256){
+        uint256 monsterId = AEMonsterContract.buyMonster(msg.sender,tokenURI);
+        AECoinContractAddress.burnFromGame(msg.sender, 100);
+        return monsterId;   
+    }
+    
+}
